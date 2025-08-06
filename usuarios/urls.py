82a6cdb2c3147989f17c0e6e9097e4e40a532c21
Mobu7p <tobiascarpintieri07@hotmail.com>
2025-08-06 11:28:55 -0300

    Twother V1

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='lista_posteos'), name='logout'),
     path('perfil/', views.ver_perfil, name='ver_perfil'),
     path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
 ]
